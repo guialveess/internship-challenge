da05fe78a9e88a83450f8e7d338fa9f351deef3a
guialveess <97guilherme.alves@gmail.com>
2025-03-21 14:18:18 -0300

    :books: docs: Atualização da Pagina de Listagem

--- app/listagem/page.tsx
+++ app/listagem/page.tsx
@@ -1,10 +1,11 @@
-'use client';  
+"use client";
 
 import { useEffect, useState } from "react";
 import axios from "axios";
-import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";  
-import { Checkbox } from "@/components/ui/checkbox";
+import { Table, TableBody, TableCell, TableRow } from "@/components/ui/table";
 import Container from "@/components/container";
+import { motion } from "framer-motion";
+import Button from "@/components/button";
 
 interface Item {
   id: number;
@@ -15,27 +16,16 @@ interface Item {
 export default function ListagemPage() {
   const [itens, setItens] = useState<Item[]>([]);
   const [loading, setLoading] = useState(true);
-  const [selectedItems, setSelectedItems] = useState<number[]>([]); // state para gerenciar os itens selecionados
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('/api/listagem');  
+      const response = await axios.get("/api/listagem");
       setItens(response.data);
     } catch (error) {
-      console.error('Erro ao carregar os dados', error);
+      console.error("Erro ao carregar os dados", error);
     } finally {
       setLoading(false);
     }
-  };
-
-  const handleCheckboxChange = (id: number) => {
-    setSelectedItems((prevSelectedItems) => {
-      if (prevSelectedItems.includes(id)) {
-        return prevSelectedItems.filter(itemId => itemId !== id);
-      } else {
-        return [...prevSelectedItems, id];
-      }
-    });
   };
 
   useEffect(() => {
@@ -43,38 +33,52 @@ export default function ListagemPage() {
   }, []);
 
   if (loading) {
-    return <div>Carregando Inscrições...</div>;
+    return (
+      <div className="flex justify-center items-center min-h-screen mx-auto max-w-lg">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 1 }}
+        >
+          Carregando Inscrições...
+        </motion.div>
+      </div>
+    );
   }
 
   return (
     <Container>
-      <div className="flex justify-center items-center min-h-screen">
+      <div className="flex flex-col justify-center items-center min-h-screen mx-auto max-w-lg">
         <div className="relative max-w-full overflow-auto rounded-md border bg-background">
-          <Table aria-label="Listagem de Itens">
-            <thead>
-              <tr>
-                <th className="py-2 px-4">Selecionar</th>
-                <th className="py-2 px-4">ID</th>
-                <th className="py-2 px-4">Nome</th>
-                <th className="py-2 px-4">Email</th>
-              </tr>
-            </thead>
-            <TableBody>
-              {itens.map((item) => (
-                <TableRow key={item.id} className="hover:bg-transparent [&>:not(:last-child)]:border-r">
-                  <TableCell className="py-2 px-4">
-                    <Checkbox 
-                    
-                      onChange={() => handleCheckboxChange(item.id)} 
-                    />
-                  </TableCell>
-                  <TableCell className="py-2 px-4">{item.id}</TableCell>
-                  <TableCell className="py-2 px-4">{item.nome}</TableCell>
-                  <TableCell className="py-2 px-4">{item.email}</TableCell>
-                </TableRow>
-              ))}
-            </TableBody>
-          </Table>
+          <div className="overflow-x-auto">
+            <Table aria-label="Listagem de Itens">
+              <thead>
+                <tr>
+                  <th className="py-2 px-4">ID</th>
+                  <th className="py-2 px-4">Nome</th>
+                  <th className="py-2 px-4">Email</th>
+                </tr>
+              </thead>
+              <TableBody>
+                {itens.map((item) => (
+                  <TableRow
+                    key={item.id}
+                    className="hover:bg-transparent [&>:not(:last-child)]:border-r"
+                  >
+                    <TableCell className="py-2 px-4">{item.id}</TableCell>
+                    <TableCell className="py-2 px-4">{item.nome}</TableCell>
+                    <TableCell className="py-2 px-4">{item.email}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </div>
+        </div>
+        <div className="w-full max-w-sm mt-6">
+          <Button href="/" className="w-full">
+            Voltar para página inicial
+          </Button>
         </div>
       </div>
     </Container>
